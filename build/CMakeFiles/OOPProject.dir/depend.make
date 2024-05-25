0f53f9e1ff071ba8a8499dd59d4fc7d2bec5c1a9
Muhammad Khuzaima Sharif <71089813+darksword990@users.noreply.github.com>
2024-05-25 21:16:11 +0500

    Delete build directory

--- build/CMakeFiles/OOPProject.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for OOPProject.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/OOPProject.dir/src/structs/GameState.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/OOPProject.dir/src/structs/GameState.cpp.o: \
-  C:\Users\user\Documents\oop-project\src\structs\GameState.cpp
